2499f44eeb51ee3c84de7d28e4f15fb9b829f031
pp <mds.skali@gmail.com>
2022-10-24 09:15:53 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+class Prg
+{
+    static void main()
+    {
+        Console.Write("Hello World");
+    }
+}
