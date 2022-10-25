cbe872975a22a6861a2f03c77f98e9267928c555
mdsfes <mds.skali@gmail.com>
2022-10-25 10:11:02 +0100

    exercice1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 class Prg
 {
-    static void main()
+    public static void Main()
     {
-        Console.Write("Hello World");
-        Console.Write("helll");
-        Console.Write("helll");
+        string nbr;
+        int N;
+        bool P = true;
+        nbr = Console.ReadLine();
+        while (!(int.TryParse(nbr, out N)))
+        {
+            nbr = Console.ReadLine();
+        };
+        for (int i = 2; i < N - 1 && P; i++)
+        {
+            if (N % i == 0)
+            {
+                P = false;
+            }
+        }
+        if (P)
+        {
+            Console.WriteLine("nbr premier");
+        }
+        else
+        {
+            Console.WriteLine("nbr non premier");
+        }
     }
 }
